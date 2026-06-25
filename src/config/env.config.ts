052ed7cfa6f6df34f8b5f35004ed0ab5fddd9fea
Adithyan <adithyanadi408@gmail.com>
2026-06-25 10:20:12 +0530

    feat:implement centralized logging using pino

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -5,7 +5,8 @@ const envSchema = z.object({
     PORT: z.coerce.number().int().min(1).max(65535).default(3000),
     DATABASE_URL: z.string().min(1, "DATABASE_URL is required"),
     JWT_SECRET: z.string().min(1, "JWT_SECRET is required"),
-    JWT_EXPIRES_IN: z.string().default("1d")
+    JWT_EXPIRES_IN: z.string().default("1d"),
+    LOG_LEVEL:z.string().min(1)
 });
 
 const parsedEnv = envSchema.safeParse(process.env);
