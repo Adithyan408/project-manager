ee6fa6e5ce040e4b9a5c5251e7503c18cc0fc8ac
Adithyan <adithyanadi408@gmail.com>
2026-06-25 17:12:54 +0530

    feat: implement task CRUD operations

---
+++ src/application/dtos/task/update-task.dto.ts
@@ -0,0 +1,16 @@
+import { z} from "zod";
+
+export const UpdateTaskDtoSchema = z.object({
+    title: z.string().trim().min(5).max(150).optional(),
+    status: z.enum(["TODO", "IN_PROGRESS", "DONE"]).optional(),
+    dueDate: z.coerce.date().optional()
+})
+.refine(
+    (data) => 
+        data.title !== undefined ||
+        data.status !== undefined ||
+        data.dueDate !== undefined,
+        {message:"Atleast one field must be provided"}
+)
+
+export type UpdateTaskDto = z.infer<typeof UpdateTaskDtoSchema>;
